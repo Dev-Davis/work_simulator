5e62e28d25e953fcc304d86ba69020dfbe2b83a9
Sean Davis <sean.dev.davis@gmail.com>
2024-11-18 23:37:13 -0600

    work and overtime features

--- timeclock.py
+++ timeclock.py
@@ -3,7 +3,7 @@ import sys
 from datetime import datetime
 
 hourly_pay = 16
-shift = 8
+shift = 9
 lunch = 1
 
 
@@ -11,36 +11,48 @@ def clock_in():
     current_time = datetime.now()
     logged_time = current_time.strftime("%H:%M")
     clocked_in = True
-    current_pay = 0
+    # current_pay = 0
     approved_ot = False
     worker_hours = 0
 
     print(f"You clocked in at {logged_time}")
 
     if clocked_in:
-        while int(worker_hours + 1) <= int(shift):
-            if worker_hours == 4:
-                print("Take your lunch")
+        while int(worker_hours) <= int(shift):
+            if worker_hours == 2 or worker_hours == 8:
+                # time.sleep(2)
+                break_time()
+                worker_hours = worker_hours + 1
+            elif worker_hours == 5:
+                # time.sleep(2)
                 start_lunch()
+                end_lunch()
                 worker_hours = worker_hours + 1
             else:
-                worker_hours = worker_hours + 1
                 # time.sleep(2)
                 current_pay = hourly_pay * worker_hours
-                print(current_pay)
-                continue
+                print(f"{current_pay} - {logged_time}")
+                worker_hours = worker_hours + 1
+                # continue
 
-        if int(worker_hours) > int(shift) - 1:
-            print("1. Would you like overtime? ")
-            print("2. Would you like to clock out? ")
+        if int(worker_hours) > int(shift):
+            overtime_input = input("Would you like overtime? y/n ")
 
-            overtime_input = input("1 or 2?")
+            if overtime_input == 'y':
+                ot_approval = input("Is overtime approved? y/n ")
+                approved_ot = True
 
-            match overtime_input:
-                case 1:
-                    print("Work overtime")
-                case 2:
-                    clock_out(logged_time)
+                match ot_approval:
+                    case 'y':
+                        print("Work overtime")
+                        overtime(hourly_pay, approved_ot)
+                    case 'n':
+                        clock_out(logged_time)
+                        sys.exit()
+            else:
+                print("Overtime denied")
+                clock_out()
+                sys.exit()
 
 
 def clock_out(logged_time):
@@ -50,7 +62,38 @@ def clock_out(logged_time):
 
 def start_lunch():
     print('Start lunch')
+    # time.sleep(30)
 
 
 def end_lunch():
+    print('End lunch')
+
+
+def break_time():
+    print("On break")
+    return None
+
+
+def overtime(pay, approved):
+
+    oth = int(1)
+    ot_hours = int(input('How many overtime hours are you working? '))
+
+    while ot_hours > 4:
+        print("You can only work 4 overtime hours")
+
+    while approved:
+        if oth <= ot_hours:
+            oth = oth + 1
+            ot_pay = pay + pay/2
+            time.sleep(2)
+            print(f"{ot_pay} - {ot_hours}")
+        else:
+            approved = False
+
+    final_ot = ot_hours * ot_pay
+    print(final_ot)
+
+
+def work():
     clock_in()
--- work.py
+++ work.py
@@ -9,19 +9,16 @@ Welcome to *Work*
 /////////////////
 """)
 time.sleep(0.7)
-print('Clock in? y/n:')
-print('1. Clock In')
+print('Clock in? y/n: \n')
+# print('1. Clock In')
 # print('2. Start Lunch')
 # print('3. End Lunch')
 # print('4. Clock Out')
 
-user_punch = input('Select an option: ')
+user_punch = input('Select an option: \n')
 
 match user_punch:
     case 'y':
-        timeclock.clock_in()
-        timeclock.start_lunch()
-        timeclock.end_lunch()
-        timeclock.clock_out()
+        timeclock.work()
     case 'n':
         print("You aren't clocked in")
